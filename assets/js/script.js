bddf3427b595271bdd4063462f06ec760494ddc3
J Litt <jdlitton2594@gmail.com>
2022-06-04 18:39:16 -0500

    quiz content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
 let startButtonEl = document.querySelector("#start");
 let quizEl = document.querySelector("#content");
 
-const quizQues = [
-    {
-    question: "Which is a string?",
-    answers: {
-        a: ""
-    }
-};]
 
 function timeLimit() {
     let timeLeft = 60;
@@ -26,24 +19,281 @@ function timeLimit() {
 }
 
 // document.body.onload = startQuizOne;
+function loadResult () {
 
-function startQuizOne(event) {
-   
+};
 
+function questionFive () {
+    let quesBoxFive = document.createElement("div");
+    quesBoxFive.className = "quiz-section";
+
+    let question = document.createElement("h1");
+    question.textContent = "Which of the following is not an event?";
+    
+    let ans1 = document.createElement("button")
+    ans1.textContent = "click";
+    ans1.id = "ans5-1";
+
+    let ans2 = document.createElement("button")
+    ans2.textContent = "hover";
+    ans2.id = "ans5-2";
+
+    let ans3 = document.createElement("button")
+    ans3.textContent = "check";
+    ans3.id = "ans5-3";
+
+    let ans4 = document.createElement("button")
+    ans4.textContent = "view";
+    ans4.id = "ans5-4";
+
+    let answers = document.createElement("ol")
+    answers.className = "quiz-answers"
+    answers.append(ans1, ans2, ans3, ans4);
+
+    quesBoxFive.append(question, answers);
+    quizEl.appendChild(quesBoxFive);
+
+    let ans1El = document.querySelector("#ans5-1")
+    let ans2El = document.querySelector("#ans5-2")
+    let ans3El = document.querySelector("#ans5-3")
+    let ans4El = document.querySelector("#ans5-4")
+    
+    ans1El.addEventListener("click", evaluate);
+    ans2El.addEventListener("click", evaluate);
+    ans3El.addEventListener("click", evaluate);
+    ans4El.addEventListener("click", evaluate);
+
+    
+    function evaluate() {
+        if (ans4El.clicked) {
+            loadResult();
+            return true; 
+    }
+    else {
+        loadResult();
+        return false;
+    }};
+};
+
+function questionFour() {
+    let quesBoxFour = document.createElement("div");
+    quesBoxFour.className = "quiz-section";
+
+    let question = document.createElement("h1");
+    question.textContent = "How do you assign a class to a newly created element?";
+    
+    let ans1 = document.createElement("button")
+    ans1.textContent = "setClass";
+    ans1.id = "ans4-1";
+
+    let ans2 = document.createElement("button")
+    ans2.textContent = "className";
+    ans2.id = "ans4-2";
+
+    let ans3 = document.createElement("button")
+    ans3.textContent = "getClass";
+    ans3.id = "ans4-3";
+
+    let ans4 = document.createElement("button")
+    ans4.textContent = "addClass";
+    ans4.id = "ans4-4";
+
+    let answers = document.createElement("ol")
+    answers.className = "quiz-answers"
+    answers.append(ans1, ans2, ans3, ans4);
+
+    quesBoxFour.append(question, answers);
+    quizEl.appendChild(quesBoxFour);
+
+    let ans1El = document.querySelector("#ans4-1")
+    let ans2El = document.querySelector("#ans4-2")
+    let ans3El = document.querySelector("#ans4-3")
+    let ans4El = document.querySelector("#ans4-4")
+    
+    ans1El.addEventListener("click", evaluate);
+    ans2El.addEventListener("click", evaluate);
+    ans3El.addEventListener("click", evaluate);
+    ans4El.addEventListener("click", evaluate);
+
+    
+    function evaluate() {
+        if (ans2El.clicked) {
+            questionFive();
+            return true; 
+    }
+    else {
+        questionFive();
+        return false;
+    }};
+};
+
+function questionThree() {
+    let quesBoxThree = document.createElement("div");
+    quesBoxThree.className = "quiz-section";
+
+    let question = document.createElement("h1");
+    question.textContent = "What is the correct way to comment?";
+    
+    let ans1 = document.createElement("button")
+    ans1.textContent = "<comment>";
+    ans1.id = "ans3-1";
+
+    let ans2 = document.createElement("button")
+    ans2.textContent = "(comment)";
+    ans2.id = "ans3-2";
+
+    let ans3 = document.createElement("button")
+    ans3.textContent = "//comment";
+    ans3.id = "ans3-3";
+
+    let ans4 = document.createElement("button")
+    ans4.textContent = "--comment";
+    ans4.id = "ans3-4";
+
+    let answers = document.createElement("ol")
+    answers.className = "quiz-answers"
+    answers.append(ans1, ans2, ans3, ans4);
+
+    quesBoxThree.append(question, answers);
+    quizEl.appendChild(quesBoxThree);
+
+    let ans1El = document.querySelector("#ans3-1")
+    let ans2El = document.querySelector("#ans3-2")
+    let ans3El = document.querySelector("#ans3-3")
+    let ans4El = document.querySelector("#ans3-4")
+    
+    ans1El.addEventListener("click", evaluate);
+    ans2El.addEventListener("click", evaluate);
+    ans3El.addEventListener("click", evaluate);
+    ans4El.addEventListener("click", evaluate);
+
+    
+    function evaluate() {
+        if (ans3El.clicked) {
+            questionFour();
+            return true; 
+    }
+    else {
+        questionFour();
+        return false;
+    }};
+};
+
+function questionTwo() {
+    let quesBoxTwo = document.createElement("div");
+    quesBoxTwo.className = "quiz-section";
+
+    let question = document.createElement("h1");
+    question.textContent = "What is the correct way to call a function?";
+    
+    let ans1 = document.createElement("button")
+    ans1.textContent = "functionName--";
+    ans1.id = "ans2-1";
+
+    let ans2 = document.createElement("button")
+    ans2.textContent = "(functionName)";
+    ans2.id = "ans2-2";
+
+    let ans3 = document.createElement("button")
+    ans3.textContent = "//functionName";
+    ans3.id = "ans2-3";
+
+    let ans4 = document.createElement("button")
+    ans4.textContent = "functionName()";
+    ans4.id = "ans2-4";
+
+    let answers = document.createElement("ol")
+    answers.className = "quiz-answers"
+    answers.append(ans1, ans2, ans3, ans4);
+
+    quesBoxTwo.append(question, answers);
+    quizEl.appendChild(quesBoxTwo);
+
+    let ans1El = document.querySelector("#ans2-1")
+    let ans2El = document.querySelector("#ans2-2")
+    let ans3El = document.querySelector("#ans2-3")
+    let ans4El = document.querySelector("#ans2-4")
+    
+    ans1El.addEventListener("click", evaluate);
+    ans2El.addEventListener("click", evaluate);
+    ans3El.addEventListener("click", evaluate);
+    ans4El.addEventListener("click", evaluate);
+
+    
+    function evaluate() {
+        if (ans4El.clicked) {
+            questionThree();
+            return true; 
+    }
+    else {
+        questionThree();
+        return false;
+    }};
+
+    // correct = true;
+    // incorrect = false;
+}
+
+function questionOne() {
     let quesBoxOne = document.createElement("div");
     quesBoxOne.className = "quiz-section";
 
-    let questionOne = document.createElement("h1");
-    questionOne.textContent = quizQues.one;
+    let question = document.createElement("h1");
+    question.textContent = "What is the name for a value that consists of text input?";
+    
+    let ans1 = document.createElement("button")
+    ans1.textContent = "string";
+    ans1.id = "answer-one";
 
-    let answersOne = document.createElement("ul");
-    // answersOne.textContent = "<li> " + quesAns.one + " </li><li> " + quesAns.two + "</li><li>" + quesAns.three + "</li><li>" + quesAns.four + "</li>";
-    answersOne.textContent = quesAns.one + quesAns.two + quesAns.three + quesAns.four;
+    let ans2 = document.createElement("button")
+    ans2.textContent = "attribute";
+    ans2.id = "answer-two";
 
-    quesBoxOne.append(questionOne, answersOne);
+    let ans3 = document.createElement("button")
+    ans3.textContent = "element";
+    ans3.id = "answer-three";
+
+    let ans4 = document.createElement("button")
+    ans4.textContent = "object";
+    ans4.id = "answer-four";
+
+    let answers = document.createElement("ol")
+    answers.className = "quiz-answers"
+    answers.append(ans1, ans2, ans3, ans4)
+
+    quesBoxOne.append(question, answers);
     quizEl.appendChild(quesBoxOne);
+
+    let ans1El = document.querySelector("#answer-one")
+    let ans2El = document.querySelector("#answer-two")
+    let ans3El = document.querySelector("#answer-three")
+    let ans4El = document.querySelector("#answer-four")
+    
+    ans1El.addEventListener("click", evaluate);
+    ans2El.addEventListener("click", evaluate);
+    ans3El.addEventListener("click", evaluate);
+    ans4El.addEventListener("click", evaluate);
+
+    
+    function evaluate() {
+        if (ans1El.clicked) {
+            questionTwo();
+            return true;
+            
+        }
+        else {
+            questionTwo();
+            return false;
+            
+        }
+    };
+
+    // correct = true;
+    // incorrect = false;
 
     // timeLimit();
 }
 
-startButtonEl.addEventListener("click", startQuizOne);
+
+
+startButtonEl.addEventListener("click", questionOne);
